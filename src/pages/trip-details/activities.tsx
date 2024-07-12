6b9736a4fef14487bb28c5b279526c3314040981
lucasmoraist <luksmnt1101@gmail.com>
2024-07-12 14:44:01 -0300

    feat: Update Activities component to fetch and display activities for a specific trip

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,12 +1,26 @@
 import { CircleCheck, Plus } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { IActivity } from "../../interface/activity";
+import { api } from "../../lib/axios";
+import { format, addDays } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface ActivitiesProps {
   toggleCreateActivityModal: () => void;
 }
 
 export function Activities({ toggleCreateActivityModal }: ActivitiesProps) {
+  const { tripId } = useParams();
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data));
+  }, [tripId]);
+
   return (
     <div className="flex-1 space-y-6">
       <div className="flex items-center justify-between">
@@ -18,34 +32,46 @@ export function Activities({ toggleCreateActivityModal }: ActivitiesProps) {
       </div>
 
       <div className="space-y-8">
-        <div className="space-y-2.5">
-          <div className="flex gap-2 items-baseline">
-            <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-            <span className="text-xs text-zinc-500">Sábado</span>
-          </div>
+        {activities.map((category) => {
+          const categoryDate = new Date(category.date);
+          
+          const newDate = addDays(categoryDate, 1);
 
-          <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada</p>
-        </div>
+          const displayDate = format(newDate, 'EEEE', { locale: ptBR });
 
-        <div className="space-y-2.5">
-          <div className="flex gap-2 items-baseline">
-            <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-            <span className="text-xs text-zinc-500">Domingo</span>
-          </div>
+          return (
+            <div key={categoryDate.toISOString()} className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia {categoryDate.getUTCDate()}
+                </span>
+                <span className="text-xs text-zinc-500">
+                  {displayDate}
+                </span>
+              </div>
 
-          <div className="space-y-2.5">
-            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-              <CircleCheck className="size-5 text-lime-300" />
-              <span className="text-zinc-100">Academia em grupo</span>
-              <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+              {category.activities.length > 0 ? (
+                <div>
+                  {category.activities.map((activity) => {
+                    return (
+                      <div key={activity.id} className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(activity.occursAt, "HH:mm")}h
+                        </span>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <p className="text-zinc-500 text-sm">
+                  Nenhuma atividade cadastrada
+                </p>
+              )}
             </div>
-            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-              <CircleCheck className="size-5 text-lime-300" />
-              <span className="text-zinc-100">Almoço</span>
-              <span className="text-zinc-400 text-sm ml-auto">12:00h</span>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
