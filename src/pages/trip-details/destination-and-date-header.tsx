e2ebf8b9ee33c72477f479555a1df2e48a5f7cc1
lucasmoraist <luksmnt1101@gmail.com>
2024-07-12 14:44:07 -0300

    chore: Update DestinationAndDateHeader component to fetch and display trip details

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,19 +1,39 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ITrip } from "../../interface/trip";
+
 
 export function DestinationAndDateHeader() {
+
+  const { tripId } = useParams()
+  const [trip, setTrip] = useState<ITrip | undefined>()
+  
+  useEffect(() => {
+    api.get(`/trips/${tripId}`)
+    .then((response) => setTrip(response.data));
+  }, [tripId])
+
+  const displayedDate = trip
+    ? format(trip?.startAt, "d ' de ' LLL")
+    .concat(' até ')
+    .concat(format(trip?.endAt, "d ' de ' LLL")) 
+    : null
+
   return (
     <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-zinc-100">Florianópolis, Brasil</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </div>
 
       <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">17 a 23 de Agosto</span>
+          <span className="text-zinc-100">{displayedDate}</span>
         </div>
 
         <div className="w-px h6 bg-zinc-800" />
