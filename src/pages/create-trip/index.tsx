db7a1f837726bd7cd27ca86c79cb53a07961b907
lucasmoraist <luksmnt1101@gmail.com>
2024-07-15 13:11:35 -0300

    chore: changing route to logo

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -91,7 +91,7 @@ export function CreateTripPage() {
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
-          <img src="/logo.svg" alt="plann.er" />
+          <img src="../../../public/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">
             Convide seus amigos e planeje sua próxima viagem!
           </p>
